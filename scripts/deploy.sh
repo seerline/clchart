e0df169951a30500ce565b2cff4f5a00d649bd9f
micjoyce <micjoyce90@gmail.com>
2018-04-17 13:23:08 +0800

    build for gh-pages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,9 @@
 npm run build
 
 cp ./dist/ClChart.js ./samples
+cp ./dist/ClChart.js.map ./samples
 
 node ./scripts/gh-pages.js
+
+rm -rf ./samples/ClChart.js
+rm -rf ./samples/ClChart.js.map
--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,5 +1,10 @@
 const ghpages = require('gh-pages')
 
-ghpages.publish('samples', (err) => {
-  console.log(err)
+ghpages.publish('samples', {
+  repo: 'seerline.github.com:seerline/clchart.git'
+}, (err) => {
+  if (!err) {
+    return console.log('deploy success')
+  }
+  console.log(`deploy error ${err}`)
 })
