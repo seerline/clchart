e62b135e9a03cae3a9a271a26ec3b7bdd1fd41b7
ZHENGDONG DING <48707400@qq.com>
2018-04-19 12:57:34 +0800

    zoom array

--- src/cl.chart.def.js
+++ src/cl.chart.def.js
@@ -70,11 +70,15 @@ export const CHART_KBAR = {
   // title: { display: 'none' },
   // scroll: {display: 'none'},
   zoomInfo: {
-    min: 1,
-    max: 26,
-    index: 2, // value = index*index + 1 [1,2,5,10,17,26]
-    value: 7 // 实际的值，如果超过10，就会index=3，如果为4，index=1 index = sqrt（value - 1）
+    index: 3,
+    list: [1, 2, 4, 5, 7, 9, 12, 15, 19]
   },
+  // zoomInfo: {
+  //   min: 1,
+  //   max: 26,
+  //   index: 2, // value = index*index + 1 [1,2,5,10,17,26]
+  //   value: 7 // 实际的值，如果超过10，就会index=3，如果为4，index=1 index = sqrt（value - 1）
+  // },
   scroll: {
     display: 'none' // none不显示
   },
