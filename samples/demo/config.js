8fb61ebfdba2ba9ed36f3c89771516e7e420ad77
ding <48707400@qq.com>
2019-04-28 19:47:06 +0800

    replace ip

--- samples/demo/config.js
+++ samples/demo/config.js
@@ -2,16 +2,16 @@
 // 从服务器获取数据部分代码
 /// ///////////////////////////
 var sisdb = {
-  // code : 'SH600600',
+  code : 'SH600600',
   // code : 'sz300344',  
-  code : 'sz300366',
+  // code : 'sz300366',
   // code : 'sz000716',
-  tradeDate : 20190425
+  tradeDate : 20190419
 } 
 
 var sisClient = new SisClient.Client({
   SocketConstructor: WebSocket,
-  // endpoint: 'ws://139.224.34.238:7329',
-  endpoint: 'ws://192.168.3.118:7329',
+  endpoint: 'ws://139.224.34.238:7329',
+  // endpoint: 'ws://192.168.3.118:7329',
 })
 
