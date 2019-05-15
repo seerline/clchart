07ce05db1e8dabb5f345b1b277bb599f149f3c26
ding <48707400@qq.com>
2019-05-15 09:17:04 +0800

    replace config.js

--- samples/demo/config.js
+++ samples/demo/config.js
@@ -2,14 +2,14 @@
 // 从服务器获取数据部分代码
 /// ///////////////////////////
 var sisdb = {
-  // code : 'SH600600',
+  code : 'SH600600',
   // code : 'sz300573',  
   // code : 'sz300366',
   // code : 'SH601216',
   // code : 'SZ002110',
   // code : 'SZ000716',
   // code : 'SH600604',
-  code : 'SH601128',
+  // code : 'SH601128',
   // code : 'SH603226',
   // code : 'SZ002238',
   tradeDate : 20190429
@@ -17,7 +17,7 @@ var sisdb = {
 
 var sisClient = new SisClient.Client({
   SocketConstructor: WebSocket,
-  // endpoint: 'ws://139.224.34.238:7329',
-  endpoint: 'ws://192.168.3.118:7329',
+  endpoint: 'ws://139.224.34.238:7329',
+  // endpoint: 'ws://192.168.3.118:7329',
 })
 
